1b1f39d38e06bbced28a04586e22586e1974ec16
Josh Davis <j123qwe@gmail.com>
2024-02-11 20:27:23 +0000

    Added package check and varible for alternate optical drive

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# ripper
+# Ripper
+
+## Packages
+
+```
+apt install abcde flac nfs-common
+```
+
+## Variables
+
+```_DEVICE=/dev/cdrom``` 
+Typically **/dev/cdrom**
+
+```_NFSIP="192.168.1.1"```
+Replace with the IP address of the NFS server.
+
+```_NFSPATH="/volume1/Music"```
+Replace with the NFS path to mount.
+
--- cd_processor.sh
+++ cd_processor.sh
@@ -3,9 +3,23 @@
 # https://linux.die.net/man/1/abcde
 # https://www.andrews-corner.org/abcde/
 
+checkApps(){
+for _PKG in ${_PACKAGES}; do
+ dpkg -s ${_PKG} &> /dev/null
+ if [ $? -eq 1 ]; then
+  echo "Please install ${_PKG}. Exiting..."
+  exit 1
+ fi
+done
+}
+
+_PACKAGES="abcde flac nfs-common"
+_DEVICE=/dev/sr1
 _NFSIP="192.168.1.1"
 _NFSPATH="/volume1/Music"
 
+
+checkApps
 sudo mkdir -p /mnt/nfs
 sudo mount -t nfs ${_NFSIP}:${_NFSPATH} /mnt/nfs
 
@@ -13,9 +27,9 @@ sudo mount -t nfs ${_NFSIP}:${_NFSPATH} /mnt/nfs
 echo 0 > /proc/sys/dev/cdrom/autoclose
 
 while true; do
-    blockdev --getsize64 /dev/cdrom 2>/dev/null
+    blockdev --getsize64 ${_DEVICE} 2>/dev/null
     if [ $? -eq 0 ]; then
-      abcde -c abcde.conf
+      abcde -c abcde.conf -d ${_DEVICE}
     fi
     echo "Waiting for the next CD..."
     sleep 5
